63414590a726a62518736a2caadd7393dae7d9c6
Anders Kalhauge <anders@kalhauge.dk>
2015-11-09 11:12:42 +0100

    Nu med fragments

---
+++ app/src/main/java/dk/cphbusiness/helloexample/GoodbyeFragment.java
@@ -0,0 +1,23 @@
+package dk.cphbusiness.helloexample;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GoodbyeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.goodbye);
+        return textView;
+    }
+
+
+}
